38c8ac907875c9f0fcbb760f0aaeb2497a0214bf
Tasmeer Haider <80003142+tasmeerhaider@users.noreply.github.com>
2026-06-29 03:48:39 +0100

    Automated QA Code Injection

--- tests/saas.spec.ts
+++ tests/saas.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Dynamic QA Automated Scan', async ({ page }) => {
-  await page.goto('https://tasmeerhaider.github.io/portfolio/');
+  await page.goto('https://tasmeerhaider.github.io/portfolio');
 
   // Verify the exact page title
   await expect(page).toHaveTitle('Homepage');
